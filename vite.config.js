3033149e896a8e37888e590bae975649f3647785
Mathavan208 <210701154@rajalakshmi.edu.in>
2025-08-18 16:42:05 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      REACT_APP_API_URL: JSON.stringify(process.env.REACT_APP_API_URL),
-      REACT_APP_RAZORPAY_KEY_ID: JSON.stringify(process.env.REACT_APP_RAZORPAY_KEY_ID),
-    },
-  },
   base: './', // Important for static builds
   build: {
     rollupOptions: {
